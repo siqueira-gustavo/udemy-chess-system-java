a86e499fca0c3cb98d764595645e4380b74c08c8
Gustavo Siqueira <guss.ns@gmail.com>
2022-09-14 10:19:10 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -23,14 +23,12 @@ public class Board {
   }
 
   public Piece piece(int row, int column) {
-    if (!positionExists(row, column))
-      throw new BoardException("Position not on the board");
+    if (!positionExists(row, column)) throw new BoardException("Position not on the board");
     return pieces[row][column];
   }
 
   public Piece piece(Position position) {
-    if (!positionExists(position))
-      throw new BoardException("Position not on the board");
+    if (!positionExists(position)) throw new BoardException("Position not on the board");
     return pieces[position.getRow()][position.getColumn()];
   }
 
@@ -50,8 +48,7 @@ public class Board {
   }
 
   public boolean thereIsAPiece(Position position) {
-    if (!positionExists(position))
-      throw new BoardException("Position not on the board");
+    if (!positionExists(position)) throw new BoardException("Position not on the board");
     return piece(position) != null;
   }
 }
